007b8a6e7b2bab229a5c6e9560a3657448aca9e2
S.m.Samrat <imsamrat24@gmail.com>
2021-03-05 19:20:39 +0600

    Final

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,10 @@ const Header = () => {
     return (
         <div className="header">
             <img src={logo} alt=""/>
-            <nav><a href="/shop">Shop</a><a href="/review">Order Review</a><a href="/manage">Manage</a></nav>
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/review">Order Review</a>
+                <a href="/inventory">Manage Inventory</a></nav>
         </div>
     );
 };
